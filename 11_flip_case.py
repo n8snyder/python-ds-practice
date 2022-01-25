335bfae7da3ba251205de035ab60d63508af68bb
= <stevenmtang1@gmail.com>
2022-01-24 17:42:27 -0800

    11 complete. Refractored for cleaner code

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,14 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    reverse_case_string = ""
+
+    for letter in phrase:
+        if letter.lower() != to_swap.lower():
+            reverse_case_string += letter
+        elif letter.islower():
+            reverse_case_string += letter.upper()
+        elif letter.isupper():
+            reverse_case_string += letter.lower()    
+        
+    return reverse_case_string
