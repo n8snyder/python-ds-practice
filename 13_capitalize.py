656c8dc2d4cfd53c4cc1f1fda5dd61f85a49d49c
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-25 11:53:24 -0800

    finished 12-16

--- 13_capitalize.py
+++ 13_capitalize.py
@@ -1,10 +1,11 @@
 def capitalize(phrase):
     """Capitalize first letter of first word of phrase.
 
-        >>> capitalize('python')
-        'Python'
+    >>> capitalize('python')
+    'Python'
 
-        >>> capitalize('only first word')
-        'Only first word'
+    >>> capitalize('only first word')
+    'Only first word'
     """
-
+    first_letter = phrase[0]
+    return first_letter.upper() + phrase[1:]
